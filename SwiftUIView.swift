fed49c7500ccbbe304ed9e6f1fea5081f614081b
Adrian <ajurczenia4219@stu.d214.org>
2023-10-24 10:47:33 -0500

    Initial Commit

---
+++ SwiftUIView.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  Grades.swiftpm
+//
+//  Created by Adrian Jurczenia on 10/24/23.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
